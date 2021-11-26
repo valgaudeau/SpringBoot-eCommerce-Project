6192871de68d76fd3dc6b83d1d4b94d99295d815
valgaudeau <valgaudeau@outlook.com>
2021-11-26 16:20:42 +0000

    second commit

---
+++ eCommerce-project/src/test/java/com/springboot/eCommerce/ECommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.eCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
