a61b8f87d0d40af8502868858f817adaad751c11
valgaudeau <valgaudeau@outlook.com>
2021-11-29 11:00:33 +0000

    jar app

--- eCommerce-project/src/main/java/com/springboot/eCommerce/ECommerceProjectApplication.java
+++ eCommerce-project/src/main/java/com/springboot/eCommerce/ECommerceProjectApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerceProjectApplication {
+public class ECommerceProjectApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(ECommerceProjectApplication.class, args);
 	}
 
