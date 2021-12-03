b92c980d6d04ad0d98c30492397fe228b1be171e
valgaudeau <valgaudeau@outlook.com>
2021-12-03 12:11:24 +0000

    disabled autoConfig for SB security, downloaded bootstrap and jquery files, building home page

--- eCommerce-project/src/main/java/com/springboot/eCommerce/ECommerceProjectApplication.java
+++ eCommerce-project/src/main/java/com/springboot/eCommerce/ECommerceProjectApplication.java
@@ -2,8 +2,10 @@ package com.springboot.eCommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// EXCLUDING SECURITY CONFIGURATION FOR NOW SO I DONT GET AUTOMATIC LOGIN PAGE REDIRECT
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) 
 public class ECommerceProjectApplication 
 {
 
